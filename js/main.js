3b24abcd4b384f605b4a48ba1c31ecfdae053231
MikheilGochitashvili <mishomishiko123@gmail.com>
2022-09-20 22:27:21 +0400

    s

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 function refreshPage() {
     location.reload()
 }
+let signPop = document.querySelector('.client-log-popup') 
+let signIn = document.querySelector('.sign-in-container')
+signPop.style.display = 'none'
+signIn.addEventListener('click', () => { 
+    let signInDiv = document.querySelector('.sign-in-div')
+    signPop.style.display = 'block'
+    signInDiv.style.display = 'block'
+    signPop.addEventListener('click', () => {
+        signPop.style.display = 'none'
+        signInDiv.style.display = 'none'
+    })
+})
 
 
